49bf166454b161eae13e349028846e182b4fb695
RobinDeng <32434871+RobinDeng@users.noreply.github.com>
2024-05-15 22:40:44 +0800

    gitignore CHANGED

---
+++ src/generated/register_types.ts
@@ -0,0 +1,8 @@
+﻿/* eslint-disable */
+import { TypeStore } from "@needle-tools/engine"
+
+// Import types
+import { idle } from "../scripts/idleBehaviour.js";
+
+// Register types
+TypeStore.add("idle", idle);
