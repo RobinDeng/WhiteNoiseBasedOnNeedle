bad7077dc94ca679c0d8f83a016ab68ec452be75
RobinDeng <32434871+RobinDeng@users.noreply.github.com>
2024-05-18 14:36:49 +0800

    test WebAudio Functions

--- src/scripts/SoundBehaviour.ts
+++ src/scripts/SoundBehaviour.ts
@@ -1,12 +1,17 @@
-import { Behaviour, serializable } from "@needle-tools/engine";
+import { AudioClipModel, Behaviour, serializable } from "@needle-tools/engine";
 import SoundManager from "./SoundManager";
 
+declare type AudioClip = string;
+
 class SoundBehaviour extends Behaviour {
-    isPlaying:boolean=true;
-    bufferSourceNode?: AudioBufferSourceNode;
-    pannerNode?: PannerNode|null = null;
+    @serializable(URL)
+    soundFileUrl?:AudioClip;
+    testUrlSth:AudioClip|null = null
+    bufferSourceNode: AudioBufferSourceNode|null=null;
+    pannerNode: PannerNode|null = null;
     audioCtx: AudioContext|null = null;
     audioBuffer: AudioBuffer|null = null;
+    gain_AsConvolver0:GainNode|null = null;
     convolver1:ConvolverNode|null = null
     convolver2:ConvolverNode|null = null
     convolver3:ConvolverNode|null = null
@@ -14,8 +19,10 @@ class SoundBehaviour extends Behaviour {
     fadeConvolverGainNode1:GainNode|null = null;
     fadeConvolverGainNode2:GainNode|null = null;
     fadeConvolverGainNode3:GainNode|null = null;
-
     audioPanner: PannerNode|null = null;
+    isPlaying:boolean=true;
+
+
     get soundManager(): SoundManager {
         return (this.context as any).soundManager;
     }
@@ -29,6 +36,9 @@ class SoundBehaviour extends Behaviour {
         const arrayBuffer = await response.arrayBuffer()
         this.audioBuffer = await this.audioCtx.decodeAudioData(arrayBuffer)
         this.mainGainNode = this.audioCtx.createGain()
+        this.fadeConvolverGainNode1 = this.audioCtx.createGain()
+        this.fadeConvolverGainNode2 = this.audioCtx.createGain()
+        this.fadeConvolverGainNode3 = this.audioCtx.createGain()
         this.audioPanner = new PannerNode(audioCtx,{
             panningModel: "HRTF",
             distanceModel: "inverse",
@@ -45,35 +55,73 @@ class SoundBehaviour extends Behaviour {
             positionY: this.gameObject.worldPosition.y,
             positionZ: this.gameObject.worldPosition.z,
         })
+        //set up ConvolverNodes
+        this.gain_AsConvolver0 = this.audioCtx.createGain()
+        this.fadeConvolverGainNode1.gain.setValueAtTime(0,this.audioCtx.currentTime)
+        this.fadeConvolverGainNode2.gain.setValueAtTime(0,this.audioCtx.currentTime)
+        this.fadeConvolverGainNode3.gain.setValueAtTime(0,this.audioCtx.currentTime)
+        this.convolver1=this.audioCtx.createConvolver()
+        const convolver1Response = await fetch("../../assets/Irs/0_8s_small.mp3")
+        const arrayBuffer1 = await convolver1Response.arrayBuffer();
+        this.convolver1.buffer = await this.audioCtx.decodeAudioData(arrayBuffer1);
+        this.convolver2=this.audioCtx.createConvolver()
+        const convolver2Response = await fetch("../../assets/Irs/2_0s_medium.mp3")
+        const arrayBuffer2 = await convolver2Response.arrayBuffer();
+        this.convolver2.buffer = await this.audioCtx.decodeAudioData(arrayBuffer2);
+        this.convolver3=this.audioCtx.createConvolver()
+        const convolver3Response = await fetch("../../assets/Irs/2_9s_large.wav")
+        const arrayBuffer3 = await convolver3Response.arrayBuffer();
+        this.convolver3.buffer = await this.audioCtx.decodeAudioData(arrayBuffer3);
         
         //connect nodes
-        if(!this.mainGainNode||!this.audioCtx||!this.pannerNode||!this.convolver1||!this.convolver2||!this.convolver3||!this.fadeConvolverGainNode1||!this.fadeConvolverGainNode2||!this.fadeConvolverGainNode3){
+        if(!this.mainGainNode||!this.audioCtx||!this.pannerNode){
             throw new Error("Node Connection Failed")
-        }else{
+        }else if(!this.convolver1||!this.convolver2||!this.convolver3||!this.fadeConvolverGainNode1||
+            !this.fadeConvolverGainNode2||!this.fadeConvolverGainNode3||!this.gain_AsConvolver0){
+                throw new Error("Convolver Setup Failed")
+        }
+        else{
             this.bufferSourceNode = this.audioCtx?.createBufferSource()
             this.bufferSourceNode.buffer = this.audioBuffer
             this.bufferSourceNode.connect(this.pannerNode)
             this.pannerNode.connect(this.mainGainNode)
+            this.mainGainNode.connect(this.gain_AsConvolver0)
             this.mainGainNode.connect(this.convolver1)
             this.convolver1.connect(this.fadeConvolverGainNode1)
             this.mainGainNode.connect(this.convolver2)
             this.convolver1.connect(this.fadeConvolverGainNode2)
             this.mainGainNode.connect(this.convolver3)
             this.convolver1.connect(this.fadeConvolverGainNode3)
+            this.gain_AsConvolver0.connect(this.audioCtx.destination)
+            this.fadeConvolverGainNode1.connect(this.audioCtx.destination)
+            this.fadeConvolverGainNode2.connect(this.audioCtx.destination)
+            this.fadeConvolverGainNode3.connect(this.audioCtx.destination)
         }
     }
+    onEnable(){
 
+    }
     start() {
         console.log(this);
+        const audioCtx = new AudioContext()
+        this.init(audioCtx,this.soundFileUrl as string)
         this.bufferSourceNode = this.audioContext.createBufferSource();
         // this.bufferSourceNode.buffer = 
         this.soundManager.addObject(this);
+        window.addEventListener('keydown',(e)=>{
+            if(e.key ==="0"){
+                this.test__0ButtonTriggered()
+            }
+        })
     }
     update() {
         if (!this.pannerNode) return;
         this.context.mainCamera?.matrix
         this.gameObject.matrix
         this.pannerNode.positionX.value;
+    }
+    test__0ButtonTriggered(){
+        this.bufferSourceNode?.start()
     }
 }
 
