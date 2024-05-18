aed0f5abdbbd9db2a704d70949491c4f60b88232
RobinDeng <32434871+RobinDeng@users.noreply.github.com>
2024-05-18 16:36:36 +0800

    commit

--- src/generated/gen.js
+++ src/generated/gen.js
@@ -2,13 +2,13 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("assets/4_23testScene.glb?v=1716020990131");
+needle_exported_files.push("assets/4_23testScene.glb?v=1716021240689");
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1716020990131");
+		needleEngine.setAttribute("hash", "1716021240689");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
